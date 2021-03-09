831054c3986a5f67ff306191a4cbfc923eaae899
Daniël <dahnjohl@gmail.com>
2021-03-09 21:11:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = function() {
+  /**
+  * This magically prints the input
+  */
   return console.log.apply(null, Array.prototype.map.call(arguments, function(item) { return item; }));
 }
